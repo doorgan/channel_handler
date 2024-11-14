1cb6ac60f3135d4b0347d715940c78821fd38273
kyleVsteger <kylesteger@gmail.com>
2024-11-14 16:15:09 -0500

    dont specify patch

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule ChannelHandler.MixProject do
     [
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:phoenix, ">= 1.6.0", optional: true, only: :test},
-      {:spark, "~> 2.2.35"}
+      {:spark, "~> 2.2.0"}
     ]
   end
 
